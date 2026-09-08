be487a5f66369f5eadc828782e5516234e07395e
Aiden <aidenhill.ar@gmail.com>
2026-09-07 22:25:00 -0500

    Made the frame, made more logic on deleting and stuff, mostly design focued
    I added a frame and gave the screen its own style, I made the button interactivity smoother and styled them more, made the page size adjust to the users screen size

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+let previousQuery1 = [];
+let previousQuery2 = [];
+let previousQuery3 = [];
+
 let query = [];
+let stringQuery;
 let answer = '';
 
 let numButtons = [];
 let firstNum = true;
-let howManyFirstNums = 0;
+
+let parenthesis = 'left';
 
 const doc = document;
+let input = doc.querySelector(".input");
 let operatorButtons = [doc.querySelector(".equals"), doc.querySelector(".plus"), doc.querySelector(".minus"), doc.querySelector(".multiply"), doc.querySelector(".divide")];
+
+let specialButtons = [doc.querySelector(".delete"), doc.querySelector(".clear"), doc.querySelector(".parenthesis"), doc.querySelector(".positive-negative")]
 
 
 function getNumButtons(arr) {
@@ -26,9 +35,30 @@ function getTotalNumber(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(arr[i]) === true) {
             arr[i] = Number(arr[i].join(""));
-        }
-    }
-}
+        };
+    };
+};
+
+function positiveNegative() {
+
+};
+
+function updateScreen(value) {
+    if (value === 'clear') {
+        input.innerHTML = "Awaiting Input<span>:</span>"
+    } else if (input.textContent === "Awaiting Input:" && value !== "delete") {
+        input.textContent = value;
+    } else if (value !== "delete") {
+        input.textContent += ' ' + value;
+    } else if (value === 'delete' && input.textContent !== "Awaiting Input:") {
+        input.textContent = stringQuery.slice(0, -2);
+        if (input.textContent.length === 0) {
+            input.innerHTML = "Awaiting Input<span>:</span>";
+        };
+    };
+    stringQuery = doc.querySelector(".input").textContent;
+    console.log(stringQuery);
+};
 
 function solveQuery(arr) {
     
@@ -45,10 +75,11 @@ numButtons.forEach(button => {
             query.push(numArr);
             firstNum = false;
         } else if (firstNum === false) {
-            query[howManyFirstNums].push(val === '.' ? '.' : Number(val));
-        }
+            query[query.length - 1].push(val === '.' ? '.' : Number(val));
+        };
         console.log(query);
-    }
+        updateScreen(button.dataset.num);
+    };
 });
 
 operatorButtons.forEach(button => {
@@ -58,15 +89,55 @@ operatorButtons.forEach(button => {
             getTotalNumber(query);
             console.log(query);
             query = [];
-            howManyFirstNums = 0;
         } else {
             let operatorArray = [button.dataset.num];
             query.push(operatorArray);
-            howManyFirstNums += 2;
-        }
+        };
         firstNum = true;
         console.log(query);
-    }
+        updateScreen(button.dataset.num);
+    };
+});
+
+specialButtons.forEach(button => {
+    button.onclick = () => {
+        if (button === doc.querySelector(".delete")) {
+            query[query.length - 1].pop();
+            updateScreen("delete");
+        } else if (button === doc.querySelector(".clear")) {
+            query = [];
+            firstNum = true;
+            parenthesis = 'left';
+            updateScreen("clear");
+        } else if (button === doc.querySelector(".parenthesis")) {
+        let parenthesisArray = [];
+        if (parenthesis === 'left' && query.length === 0) {
+            parenthesisArray = "(";
+            query.push(parenthesisArray);
+            parenthesis = 'right';
+        } else if (parenthesis === 'left') {
+            parenthesisArray = "(";
+            query.push(parenthesisArray);
+            parenthesis = 'right';
+            firstNum = true;
+        } else {
+            parenthesisArray = ")";
+            query.push(parenthesisArray);
+            parenthesis = 'left';
+            firstNum = true;
+        }
+        updateScreen(parenthesisArray);
+        } else if (button === doc.querySelector("positive-negative")) {
+
+        };
+        console.log(query);
+    };
 })
 
-
+doc.querySelector(".screen p").onclick = () => {
+    if (doc.querySelector(".starting") === null) {
+        doc.querySelector(".screen p").classList.toggle("hidden");
+    }
+    doc.querySelector(".screen p").classList.remove("starting");
+    doc.querySelector(".screen p").classList.toggle("os");
+};
